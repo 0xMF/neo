dc110381c2e050247edfe3bbd4df572beb861232
Mark Fernandes <0xMF@0xMF.com>
2023-01-11 13:30:23 -0500

    one neo to rule them all

--- neo.go
+++ neo.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -8,20 +10,55 @@ import (
 	"path/filepath"
 )
 
+var config = ".neo.json"
 var mission = "default"
 
-func main() {
-	log.Printf(string(os.Args[0]))
+type Location struct {
+	Name string `json:"name"`
+	Dest string `json:"dest"`
+	Path string `json:"path"`
+}
 
-	exe, err := filepath.EvalSymlinks(os.Args[0])
-	mission = path.Dir(exe) + "/s/" + mission
-	log.Printf("Begin: %v", err)
+func Resolve(h, p, w string) error {
+	mission = h + "/" + p + "/" + w + "/s/" + mission
 
 	mCmd := exec.Command("/bin/bash", "-c", mission)
 	mCmd.Stdin = os.Stdin
 	mCmd.Stdout = os.Stdout
 	mCmd.Stderr = os.Stderr
 	mCmd.Start()
-	err = mCmd.Wait()
+	return mCmd.Wait()
+}
+
+// parse (JSON) config and if name matching this exectuable is found, dispatch to resolver
+func Dispatch(d, e string) error {
+	var data []Location
+
+	config = d + "/" + config
+	jsonF, _ := os.Open(config)
+	defer jsonF.Close()
+	plan, _ := io.ReadAll(jsonF)
+	if json.Unmarshal(plan, &data) != nil {
+		log.Fatal("could not parse config")
+	}
+
+	for _, l := range data {
+		if e == l.Name {
+			return Resolve(d, l.Path, l.Dest)
+		}
+	}
+	log.Fatal("not ready yet.")
+	return nil
+}
+
+func main() {
+	log.Printf(string(os.Args[0]))
+
+	abs, err := filepath.EvalSymlinks(os.Args[0])
+	dir := path.Dir(abs)
+	exe := path.Base(abs)
+
+	log.Printf("Begin: %v", err)
+	err = Dispatch(dir, exe)
 	log.Printf("End  : %v", err)
 }
